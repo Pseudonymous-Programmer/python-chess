b94d3956b0fe2f803514c99fe8dfbe9e82f7af9d
Thomas Neill <tommydneill@gmail.com>
2017-01-21 20:39:05 -0800

    Removed text I/O into seperate file.

--- chesslogic.py
+++ chesslogic.py
@@ -171,9 +171,6 @@ def do(move,board):
     copy[location[1]][location[0]]  = Empty()
     return(copy)
 
-def printLocation(location):
-    chars = 'ABCDEFGH'
-    return(chars[location[0]] + str(location[1]+1))
 
 def findLegalMoves(location,board):
     piece = board[location[1]][location[0]]
@@ -188,31 +185,6 @@ def findLegalMoves(location,board):
             
             ret.append(move)
     return(ret)
-def printBoard(board,perspective):
-    chars = [['■','□'],['♚','♔'],['♛','♕'],['♜','♖'],['♝','♗'],['♞','♘'],['♟','♙']]#most normal people have light-on-dark consoles. Just NOT the color operations if I'm wrong though
-    direction = -1 if perspective else 1
-    for y in range(len(board)-1 if perspective else 0, #flips board if you're white because it's at the "top" of the array
-                   -1 if perspective else len(board),
-                   -1 if perspective else 1):
-        column = board[y]
-        pending = ''
-        for x,piece in enumerate(column):
-            if(isinstance(piece,Empty)):
-                pending += chars[0][(x+y)%2==0]
-            elif(isinstance(piece,King)):
-                pending += chars[1][not piece.white]
-            elif(isinstance(piece,Queen)):
-                pending += chars[2][not piece.white]
-            elif(isinstance(piece,Rook)):
-                pending += chars[3][not piece.white]
-            elif(isinstance(piece,Bishop)):
-                pending += chars[4][not piece.white]
-            elif(isinstance(piece,Knight)):
-                pending += chars[5][not piece.white]
-            elif(isinstance(piece,Pawn)):
-                pending += chars[6][not piece.white]
-            pending += ' '
-        print(pending)
                     
 def newBoard(move,board):
     '''
---
+++ textio.py
@@ -0,0 +1,33 @@
+'''
+simple printing functions for console-based testing
+'''
+def printLocation(location):
+    chars = 'ABCDEFGH'
+    return(chars[location[0]] + str(location[1]+1))
+
+
+def printBoard(board,perspective):
+    chars = [['■','□'],['♚','♔'],['♛','♕'],['♜','♖'],['♝','♗'],['♞','♘'],['♟','♙']]#most normal people have light-on-dark consoles. Just NOT the color operations if I'm wrong though
+    direction = -1 if perspective else 1
+    for y in range(len(board)-1 if perspective else 0, #flips board if you're white because it's at the "top" of the array
+                   -1 if perspective else len(board),
+                   -1 if perspective else 1):
+        column = board[y]
+        pending = ''
+        for x,piece in enumerate(column):
+            if(isinstance(piece,Empty)):
+                pending += chars[0][(x+y)%2==0]
+            elif(isinstance(piece,King)):
+                pending += chars[1][not piece.white]
+            elif(isinstance(piece,Queen)):
+                pending += chars[2][not piece.white]
+            elif(isinstance(piece,Rook)):
+                pending += chars[3][not piece.white]
+            elif(isinstance(piece,Bishop)):
+                pending += chars[4][not piece.white]
+            elif(isinstance(piece,Knight)):
+                pending += chars[5][not piece.white]
+            elif(isinstance(piece,Pawn)):
+                pending += chars[6][not piece.white]
+            pending += ' '
+        print(pending)
